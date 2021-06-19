c94de179835a5ebf548ff71078bc9e1bd40889bf
何苗 <50581649+HMHAHA@users.noreply.github.com>
2021-06-19 10:34:46 +0800

    attribute bullet

---
+++ DemoJP/Assets/MyScript/WindBullet.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class WindBullet : MonoBehaviourPun
+{
+    public int damage;
+    public bool isMelee;
+    public bool isRock;
+
+
+    void OnTriggerStay(Collider other)
+    {
+        if (photonView.IsMine == false && PhotonNetwork.IsConnected == true)
+        {
+            return;
+        }
+
+        if (!isMelee && other.gameObject.tag == "Player")
+        {
+            PhotonView p = PhotonView.Get(other.gameObject);
+            p.RPC("OnHealtDecRPC", RpcTarget.Others, 20);
+
+            PhotonNetwork.Destroy(this.gameObject);
+        }
+        else if(!isMelee && other.gameObject.tag == "Wall")  /* Destroy when collided with wall */
+        {
+            PhotonNetwork.Destroy(this.gameObject);
+        }
+    }
+
+
+    /* A bullet can exist for at most 10 sec, after which it will be collected */
+    public int life = 600;
+    void Update()
+    {
+        if (photonView.IsMine == false && PhotonNetwork.IsConnected == true)
+        {
+            return;
+        }
+
+        if(--life == 0){
+            PhotonNetwork.Destroy(this.gameObject);
+        }
+    }
+
+
+}
