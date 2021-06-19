2b62fd304969af97e3122c3f50c654d9bfc06e6f
何苗 <50581649+HMHAHA@users.noreply.github.com>
2021-06-19 15:48:15 +0800

    attribute2

--- DemoJP/Assets/MyScript/WaterBullet.cs
+++ DemoJP/Assets/MyScript/WaterBullet.cs
@@ -20,12 +20,23 @@ public class WaterBullet : MonoBehaviourPun
 
         if (!isMelee && other.gameObject.tag == "Player")
         {
+            /* Calculate Damage */
+            zPlayer zp = other.gameObject.GetComponent<zPlayer>();
+            if (zp.Attribute == "Fire")
+            {
+                damage *= 2;
+            }
+            else if (zp.Attribute == "Earth")
+            {
+                damage /= 2;
+            }
+
             PhotonView p = PhotonView.Get(other.gameObject);
-            p.RPC("OnHealtDecRPC", RpcTarget.Others, 20);
+            p.RPC("OnHealtDecRPC", RpcTarget.Others, damage, zp.name);
 
             PhotonNetwork.Destroy(this.gameObject);
         }
-        else if(!isMelee && other.gameObject.tag == "Wall")  /* Destroy when collided with wall */
+        else if (!isMelee && other.gameObject.tag == "Wall")  /* Destroy when collided with wall */
         {
             PhotonNetwork.Destroy(this.gameObject);
         }
