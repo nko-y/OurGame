ce956b2fec71f3349b7693858752e96e33fcf840
ankoyy <1409397061@qq.com>
2021-06-19 23:49:21 +0800

    UI version 1.0

---
+++ Assets/UI/Script/GlobalVariable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVariable : MonoBehaviour
+{
+    public static string UserName;
+    public static int[] UserWeapon = new int[3];
+}
