52f49b91b61cae0baa4c878d998542cddd73ddaf
Zang <3180104057@zju.edu.cn>
2021-06-20 19:12:02 +0800

    Delete old version for a brand new start.

--- Assets/UI/Script/GlobalVariable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GlobalVariable : MonoBehaviour
-{
-    public static string UserName;
-    public static int[] UserWeapon = new int[3];
-}
